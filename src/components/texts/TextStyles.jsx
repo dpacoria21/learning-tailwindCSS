3ce18f61d22879b30e3ca234d78fa2ceb9848cd4
Diego Pacori <dpacoria@unsa.edu.pe>
2023-08-05 14:55:22 -0500

    Se agrego efectos de linea como underline - overline - no-underline

--- src/components/texts/TextStyles.jsx
+++ src/components/texts/TextStyles.jsx
@@ -7,20 +7,23 @@ export const TextStyles = () => {
             <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4'>
                 <p className='text-left'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
             </div>
-            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4 text-slate-400'>
+            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4 text-slate-400 no-underline'>
                 <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
             </div>
             <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4 text-transparent'>
                 <p className='text-right'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
             </div>
-            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4 text-teal-400'>
-                <p className='text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
+            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4 '>
+                <p className='text-justify text-teal-400 underline'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
             </div>
-            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4 text-blue-500'>
-                <p className='text-start'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
+            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4 '>
+                <p className='text-start text-blue-500 overline'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
             </div>
-            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4 text-amber-400'>
-                <p className='text-end'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
+            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4'>
+                <p className='text-end text-amber-400 line-through'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
+            </div>
+            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4 '>
+                <p className='text-end text-emerald-400 hover:underline'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
             </div>
         </section>
     
