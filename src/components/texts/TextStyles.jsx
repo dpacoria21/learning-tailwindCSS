aa647fc2816a7807b339718fbdf4652a2247615a
Diego Pacori <dpacoria@unsa.edu.pe>
2023-08-05 13:52:06 -0500

    Se trabajo con la posicion del texto (text-xxx) y un poco de flexbox, width y padding

--- src/components/texts/TextStyles.jsx
+++ src/components/texts/TextStyles.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 
 export const TextStyles = () => {
   return (
-    <div>TextStyles</div>
+    <>
+        <section className='flex flex-col items-center w-full space-y-10 bg-slate-400 py-5'>
+            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4'>
+                <p className='text-left'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
+            </div>
+            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4'>
+                <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
+            </div>
+            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4'>
+                <p className='text-right'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
+            </div>
+            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4'>
+                <p className='text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
+            </div>
+            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4'>
+                <p className='text-start'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
+            </div>
+            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4'>
+                <p className='text-end'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
+            </div>
+        </section>
+    
+    </>
   )
 }
--- src/components/texts/Typography.jsx
+++ src/components/texts/Typography.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { LetterSpacing } from './LetterSpacing'
-import { LineClamp } from './LineClamp'
-import { LineHeigh } from './LineHeigh'
-import { ListStyle } from './ListStyle'
+
+// import { LetterSpacing } from './LetterSpacing'
+// import { LineClamp } from './LineClamp'
+// import { LineHeigh } from './LineHeigh'
+// import { ListStyle } from './ListStyle'
+import { TextStyles } from './TextStyles'
 
 export const Typography = () => {
   return (
     <>
     
-        <ListStyle />
+        <TextStyles />
     
     </>
   )
