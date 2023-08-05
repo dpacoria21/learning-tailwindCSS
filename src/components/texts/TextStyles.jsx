8b81693dc5b459e4f76535aa9a55d0508a40bdc1
Diego Pacori <dpacoria@unsa.edu.pe>
2023-08-05 15:07:05 -0500

    Se trabajo con tipos de subrayado, dotted, dashed, wavy, double, y decoradores como decoration-color decoration-size decoration, y underline-offset-4

--- src/components/texts/TextStyles.jsx
+++ src/components/texts/TextStyles.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export const TextStyles = () => {
   return (
     <>
-        <section className='flex flex-col items-center w-full space-y-10 bg-slate-400 py-5 text-fuchsia-400'>
+        <section className='flex flex-col items-center w-full space-y-10 bg-slate-400 py-5'>
             <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4'>
-                <p className='text-left'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
+                <p className='text-left  text-fuchsia-400 underline decoration-black decoration-wavy underline-offset-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
             </div>
             <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4 text-slate-400 no-underline'>
                 <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
@@ -14,16 +14,16 @@ export const TextStyles = () => {
                 <p className='text-right'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
             </div>
             <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4 '>
-                <p className='text-justify text-teal-400 underline'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
+                <p className='text-justify text-teal-400 underline decoration-double decoration-2 underline-offset-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
             </div>
             <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4 '>
-                <p className='text-start text-blue-500 overline'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
+                <p className='text-start text-blue-500 overline decoration-green-600/70 decoration-dashed decoration-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
             </div>
             <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4'>
-                <p className='text-end text-amber-400 line-through'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
+                <p className='text-end text-amber-400 line-through decoration-red-500 decoration-dotted decoration-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
             </div>
             <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4 '>
-                <p className='text-end text-emerald-400 hover:underline'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
+                <p className='text-end text-emerald-400 hover:underline '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
             </div>
         </section>
     
