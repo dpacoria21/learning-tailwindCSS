534fab610a7d3fe3cf6c25e1029b7e35f3f78852
Diego Pacori <dpacoria@unsa.edu.pe>
2023-08-05 13:56:17 -0500

    Se realzio practica con los colores de texto

--- src/components/texts/TextStyles.jsx
+++ src/components/texts/TextStyles.jsx
@@ -3,23 +3,23 @@ import React from 'react'
 export const TextStyles = () => {
   return (
     <>
-        <section className='flex flex-col items-center w-full space-y-10 bg-slate-400 py-5'>
+        <section className='flex flex-col items-center w-full space-y-10 bg-slate-400 py-5 text-fuchsia-400'>
             <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4'>
                 <p className='text-left'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
             </div>
-            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4'>
+            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4 text-slate-400'>
                 <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
             </div>
-            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4'>
+            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4 text-transparent'>
                 <p className='text-right'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
             </div>
-            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4'>
+            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4 text-teal-400'>
                 <p className='text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
             </div>
-            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4'>
+            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4 text-blue-500'>
                 <p className='text-start'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
             </div>
-            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4'>
+            <div className='w-3/4 bg-gray-200 rounded-md py-8 px-4 text-amber-400'>
                 <p className='text-end'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ab perferendis vitae voluptates dolores amet sed aliquid hic voluptatum assumenda? Ad mollitia perspiciatis saepe suscipit labore harum possimus sunt libero.</p>
             </div>
         </section>
