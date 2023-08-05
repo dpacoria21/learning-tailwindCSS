4254caf01765a2674486e868675a28795dd7729e
Diego Pacori <dpacoria@unsa.edu.pe>
2023-08-05 13:42:36 -0500

    Se practico con las listas para poder modificar su icono de la derecha

--- src/components/texts/ListStyle.jsx
+++ src/components/texts/ListStyle.jsx
@@ -2,6 +2,31 @@ import React from 'react'
 
 export const ListStyle = () => {
   return (
-    <div>ListStyle</div>
+    <>
+        <ul className='list-inside list-image-[url(./cancelar.png)]'>
+            <li>Saludos y buenos dias son esenciales</li>
+            <li>Encontre esto en tu ropero</li>
+            <li>Encontre esto en tu ropero</li>
+            <li>Encontre esto en tu ropero</li>
+        </ul>
+        <ul className='list-inside list-disc'>
+            <li>Saludos y buenos dias son esenciales</li>
+            <li>Encontre esto en tu ropero</li>
+            <li>Encontre esto en tu ropero</li>
+            <li>Encontre esto en tu ropero</li>
+        </ul>
+        <ul className="list-inside list-decimal">
+            <li>xdxdxddxdxdxdx</li>
+            <li>xdxdxddxdxdxdx</li>
+            <li>xdxdxddxdxdxdx</li>
+            <li>xdxdxddxdxdxdx</li>
+        </ul>
+        <ul className="list-inside list-[upper-roman]">
+            <li>gaaaaaaaaaaa</li>
+            <li>gaaaaaaaaaaa</li>
+            <li>gaaaaaaaaaaa</li>
+            <li>gaaaaaaaaaaa</li>
+        </ul>
+    </>
   )
 }
--- src/components/texts/Typography.jsx
+++ src/components/texts/Typography.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { LetterSpacing } from './LetterSpacing'
 import { LineClamp } from './LineClamp'
 import { LineHeigh } from './LineHeigh'
+import { ListStyle } from './ListStyle'
 
 export const Typography = () => {
   return (
     <>
     
-        <LineHeigh />
+        <ListStyle />
     
     </>
   )
