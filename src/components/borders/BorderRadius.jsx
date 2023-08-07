46f117e77ba6b465bef8beb2782e05142cbbaf0c
Diego Pacori <dpacoria@unsa.edu.pe>
2023-08-07 16:25:03 -0500

    Se estuvo practicando con los bordes y sus propiedades

--- src/components/borders/BorderRadius.jsx
+++ src/components/borders/BorderRadius.jsx
@@ -2,6 +2,31 @@ import React from 'react'
 
 export const BorderRadius = () => {
   return (
-    <div>BorderRadius</div>
+    <>
+    
+        <section className='flex flex-col flex-wrap gap-5 w-full bg-purple-500 p-7'>
+
+            <div className='flex gap-5'>
+                <div className='w-20 h-20 bg-slate-800 rounded-sm'></div>
+                <div className='w-20 h-20 bg-slate-800 rounded-md'></div>
+                <div className='w-20 h-20 bg-slate-800 rounded-lg'></div>
+                <div className='w-20 h-20 bg-slate-800 rounded-full'></div>
+            </div>
+            <div className='flex gap-5'>
+                <div className='w-20 h-20 bg-slate-800 rounded-t-sm'></div>
+                <div className='w-20 h-20 bg-slate-800 rounded-b-md'></div>
+                <div className='w-20 h-20 bg-slate-800 rounded-l-lg'></div>
+                <div className='w-20 h-20 bg-slate-800 rounded-r-full'></div>
+            </div>
+            <div className='flex gap-5'>
+                <div className='w-20 h-20 bg-slate-800 rounded-tl-sm'></div>
+                <div className='w-20 h-20 bg-slate-800 rounded-tr-md'></div>
+                <div className='w-20 h-20 bg-slate-800 rounded-bl-lg'></div>
+                <div className='w-20 h-20 bg-slate-800 rounded-br-full'></div>
+            </div>
+
+        </section>
+    
+    </>
   )
 }
--- src/components/borders/BorderSettings.jsx
+++ src/components/borders/BorderSettings.jsx
@@ -2,6 +2,33 @@ import React from 'react'
 
 export const BorderSettings = () => {
   return (
-    <div>BorderSettings</div>
+    <section className='flex flex-col flex-wrap gap-5 w-full bg-purple-500 p-7'>
+        <div className='flex gap-5'>
+            <div className='w-20 h-20 bg-slate-800 rounded-sm border'></div>
+            <div className='w-20 h-20 bg-slate-800 rounded-sm border-2 border-emerald-600'></div>
+            <div className='w-20 h-20 bg-slate-800 rounded-sm border-4 border-indigo-500/90'></div>
+            <div className='w-20 h-20 bg-slate-800 rounded-sm border-8 border-cyan-500/80'></div>
+        </div>
+        <div className='flex gap-5'>
+            <div className='w-20 h-20 bg-slate-800 rounded-sm border-t'></div>
+            <div className='w-20 h-20 bg-slate-800 rounded-sm border-l-2'></div>
+            <div className='w-20 h-20 bg-slate-800 rounded-sm border-r-4'></div>
+            <div className='w-20 h-20 bg-slate-800 rounded-sm border-b-8'></div>
+        </div>
+        <div className='flex gap-5'>
+            <div className='w-20 h-20 bg-slate-800 rounded-sm border-x-2'></div>
+            <div className='w-20 h-20 bg-slate-800 rounded-sm border-y-4'></div>
+        </div>
+        <div className='divide-y divide-cyan-700'>
+            <div className='w-20 h-20 flex justify-center items-center rounded-sm bg-slate-400 p-3 '>01</div>
+            <div className='w-20 h-20 flex justify-center items-center rounded-sm bg-slate-400 p-3 '>02</div>
+            <div className='w-20 h-20 flex justify-center items-center rounded-sm bg-slate-400 p-3 '>03</div>
+        </div>
+        <div className="flex divide-x divide-cyan-700">
+            <div className='w-20 h-20 flex justify-center items-center rounded-sm bg-slate-400 p-3 '>01</div>
+            <div className='w-20 h-20 flex justify-center items-center rounded-sm bg-slate-400 p-3 '>02</div>
+            <div className='w-20 h-20 flex justify-center items-center rounded-sm bg-slate-400 p-3 '>03</div>
+        </div>
+    </section>
   )
 }
