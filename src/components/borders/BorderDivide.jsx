d12797bdee1f9ea43c62400c5bb09d8a2fb5d68b
Diego Pacori <dpacoria@unsa.edu.pe>
2023-08-07 17:04:12 -0500

    Se practico con los outlines

--- src/components/borders/BorderDivide.jsx
+++ src/components/borders/BorderDivide.jsx
@@ -10,17 +10,17 @@ export const BorderDivide = () => {
                 <div className="w-16 h-16 bg-gray-400 flex items-center justify-center">02</div>
                 <div className="w-16 h-16 bg-gray-400 flex items-center justify-center">03</div>
             </div>
-            <div className="grid grid-cols-3 divide-x divide-red-600">
+            <div className="grid grid-cols-3 divide-x-4 divide-dotted divide-red-600">
                 <div className="w-16 h-16 bg-gray-500 flex items-center justify-center">01</div>
                 <div className="w-16 h-16 bg-gray-500 flex items-center justify-center">02</div>
                 <div className="w-16 h-16 bg-gray-500 flex items-center justify-center">03</div>
             </div>
-            <div className="grid grid-rows-3 divide-y divide-red-600">
+            <div className="grid grid-rows-3 divide-y-4 divide-double divide-red-600">
                 <div className="w-16 h-16 bg-gray-500 flex items-center justify-center">01</div>
                 <div className="w-16 h-16 bg-gray-500 flex items-center justify-center">02</div>
                 <div className="w-16 h-16 bg-gray-500 flex items-center justify-center">03</div>
             </div>
-            <div className="flex flex-col-reverse divide-y divide-y-reverse divide-red-600 hover:divide-red-200">
+            <div className="flex flex-col-reverse divide-y divide-y-reverse divide-dashed divide-red-600 hover:divide-red-200">
                 <div className="w-16 h-16 bg-gray-400 flex items-center justify-center">01</div>
                 <div className="w-16 h-16 bg-gray-400 flex items-center justify-center">02</div>
                 <div className="w-16 h-16 bg-gray-400 flex items-center justify-center">03</div>
--- src/components/borders/BorderOutline.jsx
+++ src/components/borders/BorderOutline.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export const BorderOutline = () => {
   return (
-    <div>BorderOutline</div>
+    <>
+    
+        <section className='flex flex-col flex-wrap gap-5 w-full bg-purple-300 p-7'> 
+            <div className='flex gap-5'>
+                <div className='w-20 h-20 bg-slate-800 outline outline-offset-2 outline-4 outline-cyan-400'></div>
+                <div className='w-20 h-20 bg-slate-800 outline-dashed outline-offset-4 outline-4 outline-emerald-500'></div>
+                <div className='w-20 h-20 bg-slate-800 outline-double outline-offset-1 outline-2 outline-sky-600'></div>
+                <div className='w-20 h-20 bg-slate-800 outline-dotted outline-offset-4 outline-2 outline-green-400'></div>
+            </div>
+        </section>
+    
+    </>
   )
 }
