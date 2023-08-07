fe78864733a6dd4891e2f9a228fa0900f39f57b8
Diego Pacori <dpacoria@unsa.edu.pe>
2023-08-07 16:37:38 -0500

    Se estuvo probando algunas propiedades de los border, como los colores, tipos de borders, y como podemos personalizarlos

--- src/components/borders/BorderSettings.jsx
+++ src/components/borders/BorderSettings.jsx
@@ -4,16 +4,29 @@ export const BorderSettings = () => {
   return (
     <section className='flex flex-col flex-wrap gap-5 w-full bg-purple-500 p-7'>
         <div className='flex gap-5'>
-            <div className='w-20 h-20 bg-slate-800 rounded-sm border'></div>
-            <div className='w-20 h-20 bg-slate-800 rounded-sm border-2 border-emerald-600'></div>
-            <div className='w-20 h-20 bg-slate-800 rounded-sm border-4 border-indigo-500/90'></div>
-            <div className='w-20 h-20 bg-slate-800 rounded-sm border-8 border-cyan-500/80'></div>
+            <div className='w-20 h-20 bg-slate-800 rounded-sm border border-hidden'></div>
+            <div className='w-20 h-20 bg-slate-800 rounded-sm border-2 border-emerald-600 border-dotted'></div>
+            <div className='w-20 h-20 bg-slate-800 rounded-sm border-4 border-indigo-500/90 border-double'></div>
+            <div className='w-20 h-20 bg-slate-800 rounded-sm border-8 border-cyan-500/80 border-dashed'></div>
         </div>
         <div className='flex gap-5'>
             <div className='w-20 h-20 bg-slate-800 rounded-sm border-t'></div>
             <div className='w-20 h-20 bg-slate-800 rounded-sm border-l-2'></div>
             <div className='w-20 h-20 bg-slate-800 rounded-sm border-r-4'></div>
             <div className='w-20 h-20 bg-slate-800 rounded-sm border-b-8'></div>
+        </div>
+        <div className='flex gap-5'>
+            <div className='w-20 h-20 bg-slate-800 rounded-sm border-4 border-indigo-400 border-t-indigo-700'></div>
+            <div className='w-20 h-20 bg-slate-800 rounded-sm border-4 border-indigo-400 border-b-indigo-700'></div>
+            <div className='w-20 h-20 bg-slate-800 rounded-sm border-4 border-indigo-400 border-r-indigo-700'></div>
+            <div className='w-20 h-20 bg-slate-800 rounded-sm border-4 border-indigo-400 border-l-indigo-700'></div>
+        </div>
+        <div className='flex justify-center items-center gap-5'>
+            <div className='w-20 h-20 bg-slate-800 rounded-sm border-4 border-indigo-400 hover:border-indigo-800'></div>
+        </div>
+        <div className='flex gap-5'>
+            <div className='w-20 h-20 bg-slate-800 rounded-sm border-4 border-indigo-400 border-x-indigo-700'></div>
+            <div className='w-20 h-20 bg-slate-800 rounded-sm border-4 border-indigo-400 border-y-indigo-700'></div>
         </div>
         <div className='flex gap-5'>
             <div className='w-20 h-20 bg-slate-800 rounded-sm border-x-2'></div>
