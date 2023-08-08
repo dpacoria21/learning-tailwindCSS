712f3d522b45d4298b85b685f8163ed8915a2cfc
Diego Pacori <dpacoria@unsa.edu.pe>
2023-08-08 12:25:18 -0500

    Se trabajo con propiedades de grid, pero aun falta la parte del auto-flow

--- src/components/flexbox-grid/Grid.jsx
+++ src/components/flexbox-grid/Grid.jsx
@@ -2,6 +2,30 @@ import React from 'react'
 
 export const Grid = () => {
   return (
-    <div>Grid</div>
+    <>
+    
+        <section className='flex flex-col gap-4 bg-slate-600 w-full p-4'>
+            <div className='grid grid-cols-3 gap-4 bg-slate-900 p-4'>
+                <div className='p-5 bg-fuchsia-500'></div>
+                <div className='p-5 bg-fuchsia-500'></div>
+                <div className='p-5 bg-fuchsia-500'></div>
+                <div className='p-5 col-span-2 bg-fuchsia-500'></div>
+                <div className='p-5 bg-fuchsia-500'></div>
+                <div className='p-5 bg-fuchsia-500'></div>
+                <div className='col-span-2 bg-fuchsia-500'></div>
+            </div>
+
+                {/* Tomar nota que en CSS grid las columnas empiezan desde el 0
+                    por ende nuestra columna de inicio es el 1 y el end es el 7 */}
+
+            <div className='grid grid-cols-6 bg-slate-300 p-4 gap-4'>
+                <div className='p-4 bg-sky-500 col-start-2 col-span-4'></div>
+                <div className='p-4 bg-sky-500 col-start-1 col-end-3'></div>
+                <div className='p-4 bg-sky-500 col-start-5 col-end-7'></div>
+                <div className='p-4 bg-sky-500 col-start-1 col-end-7'></div>
+            </div>
+        </section>
+    
+    </>
   )
 }
