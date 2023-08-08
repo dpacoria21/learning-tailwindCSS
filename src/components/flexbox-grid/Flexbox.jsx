9cf12c397b77f57fe7dee8b2c52d616ef135cc2b
Diego Pacori <dpacoria@unsa.edu.pe>
2023-08-08 10:27:41 -0500

    Se trabajo con los basis de flexbox

--- src/components/flexbox-grid/Flexbox.jsx
+++ src/components/flexbox-grid/Flexbox.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
 export const Flexbox = () => {
+
   return (
-    <div>Flexbox</div>
+    <>
+    
+        <section className='flex flex-col gap-5 bg-gray-300 w-full p-5'>
+            <div className='flex gap-5'>
+                <div className='bg-fuchsia-500 basis-1/2 h-14 rounded-xl flex items-center justify-center text-gray-50 font-bold text-lg'>01</div>
+                <div className='bg-fuchsia-500 basis-1/2 h-14 rounded-xl flex items-center justify-center text-gray-50 font-bold text-lg'>02</div>
+            </div>
+            <div className='flex gap-5'>
+                <div className='bg-fuchsia-500 basis-1/4 h-14 rounded-xl flex items-center justify-center text-gray-50 font-bold text-lg'>01</div>
+                <div className='bg-fuchsia-500 basis-1/4 h-14 rounded-xl flex items-center justify-center text-gray-50 font-bold text-lg'>02</div>
+                <div className='bg-fuchsia-500 basis-2/4 h-14 rounded-xl flex items-center justify-center text-gray-50 font-bold text-lg'>03</div>
+            </div>
+        </section>
+    
+    </>
   )
 }
