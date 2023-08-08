33100657b7f01256ea3c3621aebab15100991e3e
Diego Pacori <dpacoria@unsa.edu.pe>
2023-08-07 20:37:10 -0500

    Se practico el uso de rings en los botones, para que tengan animaciones y poder usar el focus del button

--- src/components/borders/BorderRing.jsx
+++ src/components/borders/BorderRing.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 export const BorderRing = () => {
   return (
-    <div>BorderRing</div>
+    <>
+    
+        <section className='flex flex-col flex-wrap gap-5 w-full bg-gray-200 p-7'> 
+                <div className='flex gap-5'>
+                    <button className='w-20 h-20 bg-slate-800 ring-offset-2 ring-1'></button>
+                    <button className='w-20 h-20 bg-slate-800 ring-offset-2 ring-2'></button>
+                    <button className='w-20 h-20 bg-slate-800 ring-offset-2 ring-4'></button>
+                    <button className='w-20 h-20 bg-slate-800 ring-offset-2 focus:ring-4'></button>
+                </div>
+                <div className='flex gap-5'>
+                    <button className='w-20 h-20 bg-slate-800 ring-2 ring-pink-300 ring-inset'></button>
+                </div>
+        </section>
+    
+    </>
   )
 }
